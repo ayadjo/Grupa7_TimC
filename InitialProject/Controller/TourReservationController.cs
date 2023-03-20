fdd69a132ef0f57ad6d9d2f0c008e63d13c59e56
tasha2812 <miljevictamara@gmail.com>
2023-03-20 15:32:46 +0100

    Moving TourReservationSevice to Services.

--- InitialProject/Controller/TourReservationController.cs
+++ InitialProject/Controller/TourReservationController.cs
@@ -4,8 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using InitialProject.Service;
 using InitialProject.Repository;
+using InitialProject.Services;
 
 namespace InitialProject.Controller
 {
