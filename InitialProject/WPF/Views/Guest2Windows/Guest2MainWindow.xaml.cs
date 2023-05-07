ee3cbb120c7152a0645b86cf6c8a1fd15166ccf2
ayadjo <andjela1108@gmail.com>
2023-05-07 15:29:17 +0200

    Guest2MainWindow modified.

--- InitialProject/WPF/Views/Guest2Windows/Guest2MainWindow.xaml.cs
+++ InitialProject/WPF/Views/Guest2Windows/Guest2MainWindow.xaml.cs
@@ -12,17 +12,36 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using InitialProject.Domain.Models;
+using System.ComponentModel;
 
 namespace InitialProject.WPF.Views.Guest2Windows
 {
     /// <summary>
     /// Interaction logic for Guest2MainWindow.xaml
     /// </summary>
-    public partial class Guest2MainWindow : Window
+    public partial class Guest2MainWindow : Window, INotifyPropertyChanged
     {
+        private string _guestFullName { get; set; }
+        public string GuestFullName
+        {
+            get => _guestFullName;
+            set
+            {
+                if (_guestFullName != value)
+                {
+                    _guestFullName = value;
+                    OnPropertyChanged("GuestFullName");
+                }
+            }
+        }
+
         public Guest2MainWindow()
         {
             InitializeComponent();
+            this.DataContext = this;
+
+            GuestFullName = SignInForm.LoggedUser.FirstName +" " + SignInForm.LoggedUser.LastName;
         }
 
         private void goToToursOverview_Click(object sender, RoutedEventArgs e)
@@ -46,5 +65,12 @@ namespace InitialProject.WPF.Views.Guest2Windows
             signInForm.Show();
             this.Close();
         }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
